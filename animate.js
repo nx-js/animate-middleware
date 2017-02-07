d2e577a6ab4c3d2bbea6e3ecb72d5f86d3835ab2
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-02-07 21:49:23 +0100

    feat: remove type

--- animate.js
+++ animate.js
@@ -33,7 +33,6 @@ function animate (elem) {
 }
 animate.$name = 'animate'
 animate.$require = ['attributes']
-animate.$type = ['component', 'content']
 module.exports = animate
 
 function enterAttribute (animation) {
